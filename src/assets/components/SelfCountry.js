8ec12606f3a8e8a18f09f08be3fd485d67fa1a86
Bhaskar Paul <paulbhaskar007@gmail.com>
2021-07-27 01:55:03 +0530

    bug fix ...

--- src/assets/components/SelfCountry.js
+++ src/assets/components/SelfCountry.js
@@ -6,47 +6,45 @@ import FloatingToTop from "./FloatingToTop";
 import { withRouter } from "react-router-dom";
 
 const SelfCountry = ({ countryCode }) => {
-    let newCountryCode = String(countryCode).toLowerCase();
-    const [allNews, setAllNews] = useState([]);
+  let newCountryCode = String(countryCode).toLowerCase();
+  const [allNews, setAllNews] = useState([]);
 
-    const getAllNews = () => {
-        axios
-            .get(
-                `https://newsapi.org/v2/top-headlines?country=${newCountryCode}&apiKey=${process.env.REACT_APP_API}`
+  const getAllNews = () => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=${newCountryCode}&apiKey=${process.env.REACT_APP_API}`
+      )
+      // .then((response) => console.log(response.data.articles))
+      .then((response) => setAllNews([...allNews, ...response.data.articles]))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getAllNews();
+  }, []);
+
+  return (
+    <div>
+      <FloatingToTop />
+      {allNews.length === 0 ? (
+        <Loader />
+      ) : (
+        allNews.map(
+          (item, idx) =>
+            item.title !== "No title" && (
+              <NewsCard
+                key={idx}
+                title={item.title}
+                description={item.description}
+                image={item.urlToImage}
+                content={item.content}
+                url={item.url}
+              />
             )
-            // .then((response) => console.log(response.data.articles))
-            .then((response) =>
-                setAllNews([...allNews, ...response.data.articles])
-            )
-            .catch((err) => console.log(err));
-    };
-
-    useEffect(() => {
-        getAllNews();
-    }, [allNews, setAllNews]);
-
-    return (
-        <div>
-            <FloatingToTop />
-            {allNews.length === 0 ? (
-                <Loader />
-            ) : (
-                allNews.map(
-                    (item, idx) =>
-                        item.title !== "No title" && (
-                            <NewsCard
-                                key={idx}
-                                title={item.title}
-                                description={item.description}
-                                image={item.urlToImage}
-                                content={item.content}
-                                url={item.url}
-                            />
-                        )
-                )
-            )}
-        </div>
-    );
+        )
+      )}
+    </div>
+  );
 };
 
 export default React.memo(withRouter(SelfCountry));
